f72ec507b71bdaa5b4bc51c51ed3c57e27a995cc
Jet Rat <jet.the.rat@gmail.com>
2016-12-03 23:33:08 +0300

    Installed FusionInventory plugin and cron. Added container run variables.

---
+++ docker-build_glpi-fusion/config_db.php
@@ -0,0 +1,12 @@
+<?php
+class DB extends DBmysql {
+
+public $dbhost     = 'DOCKER_DB_HOST';
+
+public $dbuser     = 'DOCKER_DB_USER';
+
+public $dbpassword = 'DOCKER_DB_PASS';
+
+public $dbdefault  = 'DOCKER_DB_NAME';
+
+}
--- docker-build_glpi-fusion/entrypoint.sh
+++ docker-build_glpi-fusion/entrypoint.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
 
+GLPI_DB_CONF='/var/www/glpi/config/config_db.php'
+
+if [ -z "${GLPI_DB_HOST}" ]; then GLPI_DB_HOST='glpi-db';fi
+if [ -z "${GLPI_DB_NAME}" ]; then GLPI_DB_NAME='glpi';fi
+if [ -z "${GLPI_DB_USER}" ]; then GLPI_DB_USER='glpi';fi
+if [ -z "${GLPI_DB_PASS}" ]; then GLPI_DB_PASS='suP3rp244w0rds961p1';fi
+
+sed -i "s/DOCKER_DB_HOST/${GLPI_DB_HOST}/" ${GLPI_DB_CONF};
+sed -i "s/DOCKER_DB_NAME/${GLPI_DB_NAME}/" ${GLPI_DB_CONF};
+sed -i "s/DOCKER_DB_USER/${GLPI_DB_USER}/" ${GLPI_DB_CONF};
+sed -i "s/DOCKER_DB_PASS/${GLPI_DB_PASS}/" ${GLPI_DB_CONF};
+
+
+service cron stop
+rm -f /var/run/crond.pid
+service cron start
+
+
 export APACHE_RUN_USER=glpi
 export APACHE_RUN_GROUP=glpi
 export APACHE_PID_FILE=/var/run/apache2/apache2.pid
